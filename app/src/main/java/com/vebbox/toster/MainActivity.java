cf57632e32adbd535e7e182479b4f86d53c49453
vicky dennis <dct.vignesh@gmail.com>
2020-06-24 13:34:28 +0530

    volley2

--- app/src/main/java/com/vebbox/toster/MainActivity.java
+++ app/src/main/java/com/vebbox/toster/MainActivity.java
@@ -1,10 +1,22 @@
 package com.vebbox.toster;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- tost/src/main/java/com/vebbox/tost/toast.java
+++ tost/src/main/java/com/vebbox/tost/toast.java
@@ -1,10 +1,28 @@
 package com.vebbox.tost;
 
 import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 import android.widget.Toast;
 
 public class toast {
+    RequestQueue queue;
+    JsonObjectRequest request;
+    Map<String, String> map = new HashMap<String, String>();
+
     public static void v(Context c,String s){
         Toast.makeText(c,s,Toast.LENGTH_SHORT).show();
+
     }
 }
