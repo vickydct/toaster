bc5f0fe8edf38ea25c2c9e46363f5971d6a9adc9
vicky dennis <dct.vignesh@gmail.com>
2020-06-27 17:04:50 +0530

    new

--- tost/src/main/java/com/vebbox/tost/vebbox.java
+++ tost/src/main/java/com/vebbox/tost/vebbox.java
@@ -1,70 +1,48 @@
 package com.vebbox.tost;
 
 import android.content.Context;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-
 import org.json.JSONObject;
-
 import java.util.HashMap;
 import java.util.Map;
 
-import android.util.Log;
-import android.widget.Toast;
+public class vebbox {
 
-public class toast {
+    public static void querry(final Context c,String s1,String s2){
 
-
-
-    public static void in(final Context c,String s1,String s2,String s3){
         RequestQueue queue;
         JsonObjectRequest request = null;
         Map<String, String> map1 = new HashMap<String, String>();
 
         queue = Volley.newRequestQueue(c);
         map1.put("param1", s2);
-        map1.put("param2", s3);
-        final String sk=map1.get("param1");
-        try {
-
 
         request = new JsonObjectRequest(
-                Request.Method.POST, // the request method
-                s1, // the URL
-                new JSONObject(map1), // the parameters for the php
+                Request.Method.POST,
+                s1,
+                new JSONObject(map1),
                 new Response.Listener<JSONObject>() { // the response listener
                     @Override
                     public void onResponse(JSONObject response) {
                         // here you parse the json response
-                        Toast.makeText(c,sk,Toast.LENGTH_SHORT).show();
                     }
                 },
                 new Response.ErrorListener() { // the error listener
                     @Override
                     public void onErrorResponse(VolleyError error) {
-
                       /* here you can warn the user that there
                       was an error while trying to get the json
                       information from the php  */
                     }
                 });
-        }
-        catch (Exception e1){
-            Log.e("e", String.valueOf(e1));
-            Toast.makeText(c,sk+"ddddddd",Toast.LENGTH_SHORT).show();
-        }
 
-
-
-        // executing the quere to get the json information
         queue.add(request);
-        Toast.makeText(c,sk+"sssssss",Toast.LENGTH_SHORT).show();
+
     }
 
 }
